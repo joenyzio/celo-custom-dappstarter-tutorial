a650a3eae9aa0959fe813c5e14d6d1eb72577ab7
Joe Nyzio <jdnyzio@gmail.com>
2022-03-24 23:32:01 -0400

    initial commit
    code for tutorial

---
+++ pages/index-updated.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { Tabs, Tab, Typography, Box, Link } from "@mui/material";
+import deployedContracts from "../../hardhat/deployments/hardhat_contracts.json";
+import { useContractKit } from "@celo-tools/use-contractkit";
+import {
+  StorageContract,
+  GreeterContract,
+  MessengerContract,
+  ButtonAppBar,
+} from "@/components";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+export default function App() {
+  const { network } = useContractKit();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const contracts =
+    deployedContracts[network?.chainId?.toString()]?.[
+      network?.name?.toLocaleLowerCase()
+    ]?.contracts;
+
+  return (
+    <div>
+      <ButtonAppBar />
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs">
+            <Tab label="Storage Contract" {...a11yProps(0)} />
+            <Tab label="Greeter Contract" {...a11yProps(1)} />
+            <Tab label="Messenger Contract" {...a11yProps(2)} />
+            {/* <Tab label="Another Contract" {...a11yProps(3)} /> */}
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <StorageContract contractData={contracts?.Storage} />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <GreeterContract contractData={contracts?.Greeter} />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <MessengerContract contractData={contracts?.Messenger} />
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography component="div">{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
